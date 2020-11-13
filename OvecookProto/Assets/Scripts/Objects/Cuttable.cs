ce7a725bcf1b14d4db57de069e858360ffc9a746
Pandanymous <44342138+Pandanymous@users.noreply.github.com>
2020-11-13 15:35:07 +0100

    Bonjour je suis le dossier Resources (et des changements sur droppables et cuttables)

---
+++ OvecookProto/Assets/Scripts/Objects/Cuttable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuttable : MonoBehaviour
+{
+    Droppable usedObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        usedObject = GetComponent<Droppable>();
+    }
+
+    public void Cut()
+    {
+        if (usedObject.currentLegume != Droppable.ObjectType.Assiette)
+        {
+            GameObject tempCut = Resources.Load("Cuttables/" + usedObject.currentLegume.ToString() + "_tranche") as GameObject;
+            Destroy(usedObject.inGameObject);
+            usedObject.inGameObject = Instantiate(tempCut);
+            usedObject.inGameObject.transform.parent = transform;
+
+        }
+    }
+
+}
---
+++ OvecookProto/Assets/Scripts/Objects/Droppable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Droppable : MonoBehaviour
+{
+    public enum ObjectType {Tomate,Salade,Assiette};
+    public ObjectType currentLegume;
+    public GameObject inGameObject;
+    
+    // SET LE BON ALIMENT
+    void Start()
+    {
+        GameObject tempObject = Resources.Load("Tranches/" + currentLegume.ToString()) as GameObject;
+        inGameObject = Instantiate(tempObject);
+        tempObject.transform.parent = transform;
+        tempObject.transform.localPosition = Vector3.zero;
+    }
+
+    
+}
