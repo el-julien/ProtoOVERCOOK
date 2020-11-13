2e1402d42cfeff903168dbd02d97053eea46f46a
kevin <kevin@oneliferemains.com>
2020-11-13 15:28:47 +0100

    added FoodPopZone

---
+++ OvecookProto/Assets/Scripts/FoodPopZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodPopZone : MonoBehaviour
+{
+  public GameObject popResource;
+
+  void Start()
+  {
+    
+  }
+
+  void OnMouseDown()
+  {
+    OnInteraction();
+  }
+
+  public void OnInteraction()
+  {
+    GameObject avatar = GameObject.FindGameObjectWithTag("Player");
+    GameObject resource = GameObject.Instantiate(popResource);
+    resource.transform.SetParent(avatar.transform);
+    resource.transform.position = Vector3.forward;
+  }
+}
